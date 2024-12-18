18d95f6f152ee9f793c1cefb0b05812aef63bb26
JhosephP <japoma_200@hotmail.com>
2024-12-18 00:10:17 -0500

    updated cv links

--- src/components/buttons/HomeBtns.tsx
+++ src/components/buttons/HomeBtns.tsx
@@ -7,8 +7,8 @@ type Props = {
 };
 
 const HomeBtns = ({ lang }: Props) => {
-	let ES_URL = 'https://drive.google.com/file/d/1g9Aw17jbEMU4d5ak7hIaht3b8pJsHxKI/view?usp=sharing';
-	let EN_URL = 'https://drive.google.com/file/d/1RVNnNyjNEyVO5QsCRvnKhaH6muCaaMzM/view?usp=sharing';
+	let ES_URL = 'https://drive.google.com/file/d/1HQ68NzJFhppVZnWBYzUB2fV00ZgWZk8n/view?usp=sharing';
+	let EN_URL = 'https://drive.google.com/file/d/1HQ68NzJFhppVZnWBYzUB2fV00ZgWZk8n/view?usp=sharing';
 
 	let showCV = lang === 'en' ? EN_URL : ES_URL;
 
@@ -19,7 +19,7 @@ const HomeBtns = ({ lang }: Props) => {
 					href='mailto:japoma_200@hotmail.com'
 					className='inline-block px-8 py-2 text-black font-bold text-sm rounded-md group-hover:bg-[#121212] bg-gradient-to-r from-emerald-400 to-cyan-300 transition-colors group-hover:from-transparent group-hover:to-transparent group-hover:text-white'
 				>
-					HIRE ME
+					{lang === 'en' ? 'HIRE ME' : 'CONTRATAME'}
 				</a>
 			</div>
 			<div className='p-0.5 bg-gradient-to-r from-cyan-400 to-emerald-300 rounded-md group'>
@@ -29,7 +29,7 @@ const HomeBtns = ({ lang }: Props) => {
 					rel='noreferrer'
 					className='inline-block px-8 py-2 text-white font-bold text-sm rounded-md bg-[#121212] group-hover:bg-gradient-to-r transition-all group-hover:from-cyan-400 group-hover:to-emerald-300 group-hover:text-black'
 				>
-					SEE CV
+					{lang === 'en' ? 'SEE CV' : 'VER CV'}
 				</a>
 			</div>
 		</div>
